27952d86df72dc50dc42e432a38dcaa5f7b652fa
Spichkenss <spichkens.2002@mail.ru>
2023-04-06 18:43:13 +0300

    signed

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <div className={'flex flex-col gap-3 text-center'}>
             <h1 className={'text-white text-xl'}> Сделали:</h1>
             <h1 className={'text-white text-xl'}>
-                Богачевы Егор Романович И Алена Сергеевна
+                Богачевы Егор Романович и Алена Сергеевна
             </h1>
             <MyLink href={'/calculator'} title={'Калькулятор'} />
             <MyLink href={'/discriminant'} title={'Дискриминант'} />
